e110ed71d40906170552f11e773d58ec629e5a76
Goran Banina <gbanina@gmail.com>
2017-01-25 15:38:15 +0100

    Migrations

---
+++ database/migrations/2017_01_23_125234_create_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('team', ['GE', 'GL']);
+            $table->decimal('price', 5, 2);
+            $table->enum('currency', ['eur', 'kn']);
+            $table->text('description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
